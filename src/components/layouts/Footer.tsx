e9e028e3d33712749c272a8cd09b8261ef90d22c
Francis Berger <fberger@coinshares.com>
2024-11-03 23:16:22 +0100

    feat: setup

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,18 +1,28 @@
+'use client'
+
 import { cn } from '@/utils'
 import LinkWrapper from '../common/LinkWrapper'
 import { APP_METADATA } from '@/config/app.config'
 import IconWrapper from '../common/IconWrapper'
 import { IconIds } from '@/enums'
+import { useEffect, useState } from 'react'
 
 interface FooterProps {
     className?: string
 }
 
 export default function Footer(props: FooterProps) {
+    const [isInFrame, setIsInFrame] = useState(false)
+    useEffect(() => {
+        if (typeof window !== 'undefined') return
+        const inIframe = () => window.self !== window.top
+        setIsInFrame(inIframe())
+    }, [])
+    if (!isInFrame) return null
     return (
         <div className={cn('fixed bottom-0 w-full flex justify-end text-sm z-0', props.className)}>
             <div className="flex items-end gap-4 rounded-ss-xl bg-background p-3 text-default opacity-80 backdrop-blur-sm">
-                <LinkWrapper target="_blank" href={`https://${APP_METADATA.SITE_AUTHOR}.xyz/projects/safes-trades`} className="hover:text-primary">
+                <LinkWrapper target="_blank" href={`https://${APP_METADATA.SITE_AUTHOR}.xyz/projects/better-farside`} className="hover:text-primary">
                     <IconWrapper icon={IconIds.WEBSITE} className="h-5 w-5" />
                 </LinkWrapper>
                 <LinkWrapper target="_blank" href={`https://t.me/${APP_METADATA.SOCIALS.TELEGRAM}`} className="hover:text-primary">
